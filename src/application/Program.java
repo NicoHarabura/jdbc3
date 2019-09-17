18c96c750a6b96d64471a03ebb4c6edb88452aa6
NicoHarabura <nharabura@hotmail.com>
2019-09-17 19:09:12 -0300

    Adicionar dados no mySQL

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,16 +1,63 @@
 package application;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import db.DB;
 
 public class Program {
 
 	public static void main(String[] args) {
+
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
 		
-		Connection conn = DB.getConnection();
-		DB.closeConnection();
+		Connection conn = null;
 		
-	}
-	
+		PreparedStatement st = null;
+		try {
+			conn = DB.getConnection();
+			
+			st = conn.prepareStatement(
+					"INSERT INTO seller (Name, Email, BirthDate, BaseSalary, DepartmentId) VALUES (?,?,?,?,?)"
+					,Statement.RETURN_GENERATED_KEYS);
+			
+			st.setString(1, "Elipa Maromba");
+			st.setString(2, "dandogranapaacademia@hotmail.com");
+			st.setDate(3, new Date(sdf.parse("16/09/1985").getTime()));
+			st.setDouble(4, 3000.0);
+			st.setInt(5, 4);
+			
+			int rowsaffected = st.executeUpdate();
+			
+			if (rowsaffected > 0) {
+				
+				ResultSet rs = st.getGeneratedKeys();
+				while (rs.next()) {
+					int id = rs.getInt(1);
+					System.out.println("Pronto! ID = " + id);
+				}
+				
+			} else {
+				System.out.println("Nenhuma linha alterada!");
+			}
+			
+			
+		}
+		catch (SQLException e) {
+			e.printStackTrace();
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeConnection();
+		}
+
+}
 }
